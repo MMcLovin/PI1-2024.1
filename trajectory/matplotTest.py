f0cf1dd93c36b3872dde210b9824d10e3abafd7e
MMcLovin <ChadMusashi@protonmail.com>
2024-06-04 22:37:55 -0300

    Update matplotTest.py

--- trajectory/matplotTest.py
+++ trajectory/matplotTest.py
@@ -2,9 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random as rng
 
-# Parameters
+# Robot parameters
 wheelbase = 0.1         # Wheelbase of the robot (distance between the wheels)
-turn_radius = 0.15      # Radius of the turn
+turn_radius = 0.15      # Radius of the turn (htf do we get this?)
 speed = 1               # Robot simulated speed
 
 # Simulation time
@@ -15,7 +15,9 @@ time = np.arange(0, timesteps * dt, dt)
 # Simulate turn
 trajectory_x = []
 trajectory_y = []
-theta = 0               # Initial orientation of the robot
+
+# Initial orientation of the robot
+theta = 0               
 
 # generating mock x and y coordinates for the robot
 # this will be replaced by the data collected from the sensors and stored in the database
@@ -32,15 +34,16 @@ for t in time:
     trajectory_x.append(x)
     trajectory_y.append(y)
 
-# Plot trajectory as XY dots
-plt.plot(trajectory_x, trajectory_y, 'b', markersize=2)  # Plot XY dots
+# plot() fmt param: plot trajectory as XY dots = bo
+# plot() fmt param: plot trajectory XY as a line = -b
+plt.plot(trajectory_x, trajectory_y, 'bo', markersize=2)  
 plt.xlabel('X')
 plt.ylabel('Y')
 plt.title('Simulated Turn of the Line Follower Robot')
 plt.grid(False)
 plt.axis('on')
 
-# Set the spines positions to be in the middle
+# Set the spines positions (X=0, Y=0) to be in the middle
 ax = plt.gca()
 ax.spines['left'].set_position('center')
 ax.spines['bottom'].set_position('center')
