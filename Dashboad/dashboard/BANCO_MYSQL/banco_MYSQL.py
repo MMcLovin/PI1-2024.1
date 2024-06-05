0f51e446bf67e8d68341259300df7ae401961336
CerqPaulo <163113808+CerqPaulo@users.noreply.github.com>
2024-06-05 17:28:50 -0300

    Update banco_MYSQL.py

--- Dashboad/dashboard/BANCO_MYSQL/banco_MYSQL.py
+++ Dashboad/dashboard/BANCO_MYSQL/banco_MYSQL.py
@@ -1,19 +1,29 @@
 import mysql.connector
 from conexao import criar_conexao, fechar_conexao
+import random
 
-def insere_usuario(con, idCarrinho, nome, velocidade, aceleracao, consumoBat):
+def insere_usuario(con, numPercurso, velEsquerda, velDireita , aceleracaoX, aceleracaoY, aceleracaoZ, consumoBat, tempoPercuso, giroscopioX, giroscopioY, giroscopioZ, RPM):
     cursor = con.cursor()
-    sql = "INSERT INTO CARRINHO (idCarrinho, nome, velocidade, aceleracao, consumoBat) values (%s, %s, %s, %s, %s)"
-    valores = (idCarrinho, nome, velocidade, aceleracao, consumoBat)
+    sql = "INSERT INTO CARRINHO (numPercurso, velEsquerda, velDireita , aceleracaoX, aceleracaoY, aceleracaoZ, consumoBat, tempoPercuso, giroscopioX, giroscopioY, giroscopioZ, RPM) values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+    valores = (numPercurso, velEsquerda, velDireita , aceleracaoX, aceleracaoY, aceleracaoZ, consumoBat, tempoPercuso, giroscopioX, giroscopioY, giroscopioZ, RPM)
     cursor.execute(sql, valores)
     cursor.close()
     con.commit()
 
+aleatoria = 12
+
+def ale():
+    # Gerando uma tupla com 12 números de ponto flutuante aleatórios entre 0 e 1
+    tupla = tuple(random.uniform(0, 1) for _ in range(12))
+    return tupla
+
 
 def main():
     con = criar_conexao("localhost", "root", "34841984Full@", "ProjetoPI1")
-    insere_usuario(con,'4', 'seguidor3', '12','200','40.5')
-    fechar_conexao(con)
+    for i in range(aleatoria):
+        k = ale
+        insere_usuario(con, ale[1], ale[2], ale[3], ale[4], ale[5], ale[6], ale[7], ale[8], ale[9], ale[10])
+        fechar_conexao(con)
 
 
 if __name__ == "__main__":
