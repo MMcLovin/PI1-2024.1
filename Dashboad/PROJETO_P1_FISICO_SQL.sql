617d05a53cd99b52d3aef564b9f3e0ced7bb0c6a
CerqPaulo <163113808+CerqPaulo@users.noreply.github.com>
2024-06-05 17:32:14 -0300

    Update PROJETO_P1_FISICO_SQL.sql

--- Dashboad/PROJETO_P1_FISICO_SQL.sql
+++ Dashboad/PROJETO_P1_FISICO_SQL.sql
@@ -1,10 +1,18 @@
-CREATE DATABASE ProjetoPI1;
+CREATE DATABASE IF NOT exists projetopi1;
 use ProjetoPI1;
 
+
 CREATE TABLE CARRINHO(
-	idCarrinho INT primary key not null,
-    nome VARCHAR(30),
-    velocidade DECIMAL(5,2),
-    aceleracao DECIMAL(5,2),
-    consumoBat DECIMAL(10,2)
-) engine = InnoDB;
+    numPercurso INT,
+    velEsquerda DECIMAL(5,2),
+    velDireita DECIMAL(5,2),
+    aceleracaoX DECIMAL(5,3),
+    aceleracaoY DECIMAL(5,3),
+    aceleracaoZ DECIMAL(5,3),
+    consumoBat DECIMAL(10,2),
+    tempoPercuso TIME,
+    giroscopioX DECIMAL(5,3),
+    giroscopioY DECIMAL(5,3),
+    giroscopioZ DECIMAL(5,3),
+    RPM INT
+);
