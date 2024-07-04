9c9c8c50b046df5d6460ade8c1af84124993461c
CerqPaulo <163113808+CerqPaulo@users.noreply.github.com>
2024-07-04 09:14:53 -0300

    Update db_service.py

--- Program/db_service.py
+++ Program/db_service.py
@@ -6,6 +6,7 @@ from conexao import fetch_data
 
 # Responsável por criar consulta e retornar Dataframe
 
+# Funções para obter dados do banco de dados
 def get_aceleracao(con):
     consulta = "SELECT indece, numPercurso, aceleracaoX, aceleracaoY, aceleracaoZ FROM CARRINHO ORDER BY indece;"
     dados = fetch_data(con, consulta)
@@ -15,15 +16,12 @@ def get_aceleracao(con):
 
 def get_velocidade(con):
     consulta = "SELECT indece, numPercurso, velEsquerda, velDireita FROM CARRINHO ORDER BY indece;"
-
-    dados = conexao.fetch_data(con, consulta)
-    lista_resultados = [[float(item) if isinstance(item, Decimal) else item for item in linha]
-            for linha in dados
-    ]
-
+    dados = fetch_data(con, consulta)
+    lista_resultados = [[float(item) if isinstance(item, Decimal) else item for item in linha] for linha in dados]
     df = pd.DataFrame(lista_resultados, columns=["indece", "numPercurso", "velEsquerda", "velDireita"])
-
     return df
+
+
 #def get_velocidade(con):
 #def get_velocidade(con):
 #def get_velocidade(con):
